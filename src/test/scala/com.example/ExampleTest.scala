e3d093f130b256942690c3732e14af08349012b7
Yaroslav Ulanovych <y.ulanovych@thenewmotion.com>
2013-03-13 07:24:09 +0200

    configured tests running with scalatest, included test example

---
+++ src/test/scala/com.example/ExampleTest.scala
@@ -0,0 +1,12 @@
+package com.example
+
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+import org.junit.runner.RunWith
+
+@RunWith(classOf[JUnitRunner])
+class ExampleTest extends FunSuite {
+  test("test") {
+    assert(true)
+  }
+}
