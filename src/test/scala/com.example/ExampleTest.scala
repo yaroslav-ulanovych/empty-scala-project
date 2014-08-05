1a44ab0b67402bb1aec94e3b57d1dc3a73178f57
Yaroslav Ulanovych <Yaroslav_Ulanovych@epam.com>
2014-08-05 17:08:52 +0300

    update libraries and maven plugins, add scalatest maven plugin

--- src/test/scala/com.example/ExampleTest.scala
+++ src/test/scala/com.example/ExampleTest.scala
@@ -1,10 +1,7 @@
 package com.example
 
 import org.scalatest.FunSuite
-import org.scalatest.junit.JUnitRunner
-import org.junit.runner.RunWith
 
-@RunWith(classOf[JUnitRunner])
 class ExampleTest extends FunSuite {
   test("test") {
     assert(true)
